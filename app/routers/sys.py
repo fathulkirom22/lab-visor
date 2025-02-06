48b4716ebc6b993c62767b20dc2ae108a5fdfb24
fathulkirom22 <hello.kirom@gmail.com>
2025-02-06 16:12:03 +0700

    feat: add information and api

--- app/routers/sys.py
+++ app/routers/sys.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import asc, desc
 from app.models import SysDataTracker
 from app.database import get_db
-from app.responses import ErrorResponse, SysDataTrackerResponse
+from app.responses import ErrorResponse, SysDataTrackerResponse, BaseResponse
+import requests
 
 router = APIRouter(
     prefix="/sys",
@@ -38,6 +39,31 @@ async def get_sys_data_tracker(
             total_pages=total_pages,
             data=jsonable_encoder(data)
         )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, 
+            detail=jsonable_encoder(ErrorResponse(message=str(e)))
+        )
+
+@router.get("/uptime", response_model=Union[BaseResponse, ErrorResponse])
+def get_sys_uptime():
+    try:    
+        with open('/proc/uptime') as f: uptime = float(f.read().split()[0])
+        hours = int(uptime // 3600)
+        minutes = int((uptime % 3600) // 60)
+        return BaseResponse(data={"hours": hours, "minutes": minutes, "text":f"{hours} hours {minutes} minutes"})
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, 
+            detail=jsonable_encoder(ErrorResponse(message=str(e)))
+        )
+    
+@router.get("/info", response_model=Union[BaseResponse, ErrorResponse])
+def get_sys_uptime():
+    try:    
+        response = requests.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/info')
+        data = response.json()
+        return BaseResponse(data=data)
     except Exception as e:
         raise HTTPException(
             status_code=500, 
