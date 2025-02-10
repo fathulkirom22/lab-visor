681056ee9259c11e09e47c88a4f847c7b4046530
fathulkirom22 <hello.kirom@gmail.com>
2025-02-10 14:55:42 +0700

    fix: update typo

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
-from starlette.responses import JSONResponse
-from starlette.middleware.base import RequestResponseEndpoint, BaseHTTPMiddleware
-from .responses import ErrorResponse
+from starlette.responses import HTMLResponse
 from .routers import home
 from .routers.api import api_router
 from .startup import lifespan
-import htmlmin
 
 app = FastAPI(lifespan=lifespan)
 
@@ -31,8 +26,7 @@ async def error_handling_middleware(request: Request, call_next):
         response = await call_next(request)
         return response
     except Exception as e:
-        print(f"Terjadi kesalahan: {e}")  # Catat kesalahan
-        return JSONResponse(status_code=500, content=jsonable_encoder(ErrorResponse(message=str(e))))
+        return HTMLResponse(status_code=500, content=f"Error: {str(e)}")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
--- app/startup.py
+++ app/startup.py
@@ -11,7 +11,7 @@ interval = 900
 
 def track_resource_usage():
     with Session(engine) as session:
-        # Simpan data ke database
+        # Save new data
         cpu_percent = psutil.cpu_percent(interval=1) 
         memory_percent = psutil.virtual_memory().percent
         data = SysDataTracker(cpu=cpu_percent, memory=memory_percent)
@@ -19,15 +19,15 @@ def track_resource_usage():
         session.commit()
         session.refresh(data)
 
-        # Hapus data jika melebihi batas
-        nama_tabel = "sysdatatracker"  # Contoh
-        kolom_timestamp = "created_at"  # Contoh
+        # Delete old data
+        table_name = "sysdatatracker"
+        timestamp_column = "created_at"
         limit = 1000
         row = session.exec(select(func.count()).select_from(SysDataTracker)).one()
         if row > limit:
             count_delete = row - limit
-            query_delete = text(f"DELETE FROM {nama_tabel} ORDER BY {kolom_timestamp} ASC LIMIT {count_delete}")
-            session.exec(query_delete)  # Nilai parameter sebagai tuple
+            query_delete = text(f"DELETE FROM {table_name} ORDER BY {timestamp_column} ASC LIMIT {count_delete}")
+            session.exec(query_delete)
             session.commit()
 
 @asynccontextmanager
